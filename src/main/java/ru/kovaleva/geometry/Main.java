c44cb882faf66dd507f483a7ee9ec973eddcac6c
ElenaKovaleva <lenshemeta@gmail.com>
2025-09-29 09:27:46 +0300

    added dependency

--- src/main/java/ru/kovaleva/geometry/Main.java
+++ src/main/java/ru/kovaleva/geometry/Main.java
@@ -2,6 +2,20 @@ package ru.kovaleva.geometry;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+        Shape triangle = new Triangle(3 ,4, 5);
+
+        print("круг", circle);
+        System.out.println();
+        print("прямоугольник", rectangle);
+        System.out.println();
+        print("треугольник", triangle);
+    }
+
+    private static void print(String name, Shape shape) {
+        System.out.println(name);
+        System.out.println("площадь " + shape.getArea());
+        System.out.println("периметр " + shape.getPerimeter());
     }
 }
