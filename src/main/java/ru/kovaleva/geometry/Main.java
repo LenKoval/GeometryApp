78c265ea90809c77b2e3a8c91cd7ab048d50aac5
ElenaKovaleva <lenshemeta@gmail.com>
2025-09-29 11:51:01 +0300

    added new function ThreeDimensionalShapes

--- src/main/java/ru/kovaleva/geometry/Main.java
+++ src/main/java/ru/kovaleva/geometry/Main.java
@@ -6,15 +6,22 @@ public class Main {
         Shape rectangle = new Rectangle(4, 6);
         Shape triangle = new Triangle(3 ,4, 5);
 
+        System.out.println();
         print("круг", circle);
         System.out.println();
         print("прямоугольник", rectangle);
         System.out.println();
         print("треугольник", triangle);
+        System.out.println();
 
         System.out.println("сантиметры: " + GeometryUtils.squareMetersToSquareCentimeters(circle.getArea()));
-
         System.out.println("сравнение фигур: " + GeometryUtils.compareByArea(circle, rectangle));
+        System.out.println();
+
+        Sphere sphere = new Sphere(3);
+        Cube cube = new Cube(2);
+        System.out.println("объем сферы: " + sphere.getVolume() + ", площад: " + sphere.getSurfaceArea());
+        System.out.println("объем куба: " + cube.getVolume() + ", площадь: " + cube.getSurfaceArea());
     }
 
     private static void print(String name, Shape shape) {
